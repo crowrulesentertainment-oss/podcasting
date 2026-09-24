befded5b9503a880bf2fdf06a164b3fec7bdc618
CrowRules Entertainment <crowrulesentertainment@gmail.com>
2026-09-24 04:55:19 -0700

    rebuild: unified platform configuration

--- js/config.js
+++ js/config.js
@@ -1,5 +1,7 @@
 window.CROW_CONFIG={
   supabaseUrl:"https://cevylpnoexugwgygvtgu.supabase.co",
   supabaseKey:"sb_publishable_AdfM5y6RqvF3tbvEVzDZSg_JuGTQLD-",
-  siteUrl:"https://crowrulesentertainment-oss.github.io/podcasting/"
+  siteUrl:"https://crowrulesentertainment-oss.github.io/podcasting/",
+  stripePublishableKey:"",
+  stripeFunctionsBase:"/functions/v1/"
 };
