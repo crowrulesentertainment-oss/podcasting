2fae8abe26b9fc16b36ed8e7f5e60ee3c7a7d6ec
CrowRules Entertainment <crowrulesentertainment@gmail.com>
2026-09-24 04:30:36 -0700

    Fix Supabase client configuration for Creator Studio

--- js/config.js
+++ js/config.js
@@ -1,1 +1,5 @@
-window.CROW_CONFIG={supabaseUrl:"https://cevylpnoexugwgygvtgu.supabase.co",supabaseKey:"sb_publishable_AdfM5y6RqvF3tbvEVzDZSg_JuGTQLD-",siteUrl:"https://crowrulesentertainment-oss.github.io/podcasting"};
+window.CROW_CONFIG={
+  supabaseUrl:"https://cevylpnoexugwgygvtgu.supabase.co",
+  supabaseKey:"sb_publishable_AdfM5y6RqvF3tbvEVzDZSg_JuGTQLD-",
+  siteUrl:"https://crowrulesentertainment-oss.github.io/podcasting/"
+};
